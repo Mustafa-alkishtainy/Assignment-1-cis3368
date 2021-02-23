3855c1307135d27f57f92bbb7b6c44e59cff6097
Mustafa-alkishtainy <s6oofee@gmail.com>
2021-02-22 19:00:02 -0600

    Update main.py
    print all contacts names by alpha order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ##Mustafa Al-kishtainy
-##professor Otoo 3368
+##professor Oto CIS3368
 
 import mysql.connector
 from mysql.connector import Error
@@ -41,6 +41,7 @@ def read_query(connection, query):
     except Error as e:
         print(f"the error '{e}' occured")
 
+#connects to workbench and AWS
 connection= create_connection("cis3368v1.cl3c9tgm8sn0.us-east-2.rds.amazonaws.com","admin","Daguy.jason.com","cis3368v1db")
 
 
@@ -64,8 +65,7 @@ while choice != 'q':
     if choice == 'a':
         contactDetail= input("Enter contact details: ")
         creationDate = dt.date.today() ## automatically adds todays date 
-        add_contact= "INSERT INTO contacts (contactDetails, creationDate) VALUES ('%s', '%s')" % (contactDetail, creationDate)
-
+        add_contact= "INSERT INTO contacts (contactDetails, creationDate) VALUES ('%s', '%s')" % (contactDetail, creationDate)    
         execute_query(connection,add_contact)   
     ## DELETE CONTACT ##         
     elif choice == 'd':
@@ -76,18 +76,27 @@ while choice != 'q':
     elif choice == 'u':
         contact_id = input("Enter the contact you would like to edit: ")
         new_details = input("Enter the new contact deteails: ")
-
         update_contact= "UPDATE contacts SET contactDetails = '%s' WHERE id = '%s' " %(new_details, contact_id)
         execute_query(connection, update_contact)
+    ## PRINT CONTACT NAME BY ALPHABETICAL ORDER ##
     elif choice == 'b':
-        print('b')
+        select_contacts= "SELECT * FROM contacts ORDER BY contactDetails"
+        contacts =read_query(connection, select_contacts)
+        print(" ID" + "   DETAILS"+"       CREATION DATE")
+        print("------------------------------------")
+        for contact in contacts:
+            Id= contact[0]
+            contactDetail = contact[1]
+            creationDate= contact[2]
+            
+            print("{:^5} {:^5} {:>15}".format(str(Id), contactDetail, str(creationDate) )) 
     elif choice == 'c':
         print('c')
     ## OUTPUTS ALL CONTACTS ##
     elif choice =='o': 
         select_contacts= "SELECT * FROM contacts"
         contacts =read_query(connection, select_contacts)
-        print("  ID" + "   DETAILS"+"       CREATION DATE")
+        print(" ID" + "   DETAILS"+"       CREATION DATE")
         print("------------------------------------")
         for contact in contacts:
             Id= contact[0]
